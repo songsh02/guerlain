0eaab239024f25354aef0688330e8478ec70addd
songsh02 <songsh02@naver.com>
2025-11-26 17:58:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var nav = document.querySelector('nav');
+    var menuBtn = document.querySelector('.menu_button');
+    var closeBtn = document.querySelector('.close_button');
 
+    menuBtn.addEventListener('click', function() {
+        nav.style.opacity = '1';
+        nav.style.display = 'block';
+        nav.style.transition = 'opacity 0.3s ease-out';
+    });
+
+    closeBtn.addEventListener('click', function() {
+        nav.style.opacity = '0';
+        setTimeout(function() {
+            nav.style.display = 'none';
+        }, 500); 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        spaceBetween: 24
+    });
+});
